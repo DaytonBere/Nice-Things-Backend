e40873977e271ac2b243ea8ee3873176bb419285
daytonbere <daytonbere@gmail.com>
2023-04-11 15:56:08 -0500

    Deploy test

--- middleware/CORS.go
+++ middleware/CORS.go
@@ -10,7 +10,7 @@ func CORSMiddleware() gin.HandlerFunc {
     return func(c *gin.Context) {
         origin := c.Request.Header.Get("Origin")
         if origin != "" && (origin == "http://localhost:3000"  || origin == "https://assistnicethings.com") {
-            c.Writer.Header().Set("Access-Control-Allow-Origin", origin)
+            c.Writer.Header().Set("Access-Control-Allow-Origin", "*")
             c.Writer.Header().Set("Access-Control-Allow-Credentials", "true")
             c.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization")
             c.Writer.Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, PATCH")
