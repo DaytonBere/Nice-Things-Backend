2c5c1328dfa9d37d268584fff36c933a84bc100c
daytonbere <daytonbere@gmail.com>
2023-04-11 15:04:29 -0500

    Made origin secure

--- middleware/CORS.go
+++ middleware/CORS.go
@@ -9,7 +9,7 @@ import (
 func CORSMiddleware() gin.HandlerFunc {
     return func(c *gin.Context) {
         origin := c.Request.Header.Get("Origin")
-        if origin != "" && (origin == "http://localhost:3000"  || origin == "http://assistnicethings.com") {
+        if origin != "" && (origin == "http://localhost:3000"  || origin == "https://assistnicethings.com") {
             c.Writer.Header().Set("Access-Control-Allow-Origin", origin)
             c.Writer.Header().Set("Access-Control-Allow-Credentials", "true")
             c.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization")
